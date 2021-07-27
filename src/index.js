cb235a7fa928d91a3e6febe69c63a5ce63ce0177
Drxl95 <uebelhorr@gmail.com>
2021-07-26 23:17:06 -0500

    added top of page button

--- src/index.js
+++ src/index.js
@@ -42,9 +42,36 @@ function createImgs(images) {
     dogImgs.addClass("dogPic")
     imageContainer.append(dogImgs);
 }
+
+//deletes pic if dbl tapped
 $(document).on('dblclick', ".dogPic", function () {
     $(this).remove();
 })
+
+
+
+
+//back to top button
+jQuery(document).ready(function () {
+    var offset = 220;
+    var duration = 500;
+    jQuery(window).scroll(function () {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+
+    jQuery('.back-to-top').click(function (event) {
+        event.preventDefault();
+        jQuery('html, body').animate({ scrollTop: 0 }, duration);
+        return false;
+    })
+});
+
+
+
 
 
 
