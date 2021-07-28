c72a6fc00bd4de46475f8fbe29ab746881ab1608
Drxl95 <uebelhorr@gmail.com>
2021-07-28 10:22:38 -0500

    top text, top of page button
    -remvoed top page text
    -changed top of page button code

--- src/index.js
+++ src/index.js
@@ -60,23 +60,43 @@ $("#breeds").on("change", "select", function () {
 });
 
 //back to top button
-jQuery(document).ready(function () {
-    var offset = 220;
-    var duration = 500;
-    jQuery(window).scroll(function () {
-        if (jQuery(this).scrollTop() > offset) {
-            jQuery('.back-to-top').fadeIn(duration);
-        } else {
-            jQuery('.back-to-top').fadeOut(duration);
-        }
-    });
+// jQuery(document).ready(function () {
+//     var offset = 220;
+//     var duration = 500;
+//     jQuery(window).scroll(function () {
+//         if (jQuery(this).scrollTop() > offset) {
+//             jQuery('.back-to-top').fadeIn(duration);
+//         } else {
+//             jQuery('.back-to-top').fadeOut(duration);
+//         }
+//     });
 
-    jQuery('.back-to-top').click(function (event) {
-        event.preventDefault();
-        jQuery('html, body').animate({ scrollTop: 0 }, duration);
-        return false;
-    })
-});
+//     jQuery('.back-to-top').click(function (event) {
+//         event.preventDefault();
+//         jQuery('html, body').animate({ scrollTop: 0 }, duration);
+//         return false;
+//     })
+// });
+
+//Get the button
+var mybutton = document.querySelector(".top_link_position");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 
 
