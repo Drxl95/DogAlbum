9d61138196f30ad3a51fc64436d2a50eed93fc56
Drxl95 <uebelhorr@gmail.com>
2021-07-28 10:26:34 -0500

    removed old js of top of page button

--- src/index.js
+++ src/index.js
@@ -59,24 +59,6 @@ $("#breeds").on("change", "select", function () {
     }, 800)
 });
 
-//back to top button
-// jQuery(document).ready(function () {
-//     var offset = 220;
-//     var duration = 500;
-//     jQuery(window).scroll(function () {
-//         if (jQuery(this).scrollTop() > offset) {
-//             jQuery('.back-to-top').fadeIn(duration);
-//         } else {
-//             jQuery('.back-to-top').fadeOut(duration);
-//         }
-//     });
-
-//     jQuery('.back-to-top').click(function (event) {
-//         event.preventDefault();
-//         jQuery('html, body').animate({ scrollTop: 0 }, duration);
-//         return false;
-//     })
-// });
 
 //Get the button
 var mybutton = document.querySelector(".top_link_position");
